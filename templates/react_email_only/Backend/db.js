208b42904c247d0935ea5a5fadfe24e1ef976b6f
NischayB99 <c317551@gmail.com>
2025-06-12 23:03:00 +0530

    Bug fix

--- templates/react_email_only/Backend/db.js
+++ templates/react_email_only/Backend/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
